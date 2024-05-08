662985bfd33c56c3e4fa1a07d60980dff171984e
Deepak Underiya <underiyadeepak@gmail.com>
2024-05-08 23:43:40 +0530

    done

--- backend/middleware/auth.middleware.ts
+++ backend/middleware/auth.middleware.ts
@@ -1,21 +1,38 @@
-import { NextApiRequest, NextApiResponse } from "next";
-const jwt = require("jsonwebtoken");
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
 
-const authenticate =
-  (handler: any) => async (req: NextApiRequest, res: NextApiResponse) => {
-    const token = req.headers.authorization?.split(" ")[1];
+interface DecodedToken {
+  userId: string;
+  role: string;
+}
 
+const auth = async (req: Request, res: Response, next: NextFunction) => {
+  const token = req.headers.authorization;
+
+  try {
     if (!token) {
-      return res.status(401).json({ message: "Unauthorized" });
+      return res
+        .status(401)
+        .json({ message: "Authorization token is missing" });
     }
 
-    try {
-      const decoded = jwt.verify(token, process.env.JWT_SECRET);
-      req.userId = (decoded as { userId: string }).userId;
-      return handler(req, res);
-    } catch (error) {
-      return res.status(401).json({ message: "Unauthorized" });
-    }
-  };
+    jwt.verify(token, "masai", (err, decoded: any) => {
+      if (err) {
+        return res.status(401).json({ message: "Invalid token" });
+      }
 
-export default authenticate;
+      if (decoded) {
+        const { userId } = decoded as DecodedToken;
+        console.log(decoded);
+
+        next();
+      } else {
+        return res.status(401).json({ message: "Invalid token" });
+      }
+    });
+  } catch (error) {
+    res.status(400).json({ message: "Bad request", error });
+  }
+};
+
+export { auth };
