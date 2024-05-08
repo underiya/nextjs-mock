23d7697d656f1a8c2d95514503932a79e7d25f95
Deepak Underiya <underiyadeepak@gmail.com>
2024-05-08 23:56:34 +0530

    done

--- src/components/Register.ts
+++ src/components/Register.ts
@@ -1,27 +1,27 @@
-import { NextApiRequest, NextApiResponse } from "next";
-const bcrypt = require("bcrypt");
+// import { NextApiRequest, NextApiResponse } from "next";
+// const bcrypt = require("bcrypt");
 
-import userModel from "../../backend/models/user.model";
-import { connection } from "@/lib/db";
+// import userModel from "../../backend/models/user.model";
+// import { connection } from "@/lib/db";
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  const { name, email, password } = req.body;
+// export default async function handler(
+//   req: NextApiRequest,
+//   res: NextApiResponse
+// ) {
+//   const { name, email, password } = req.body;
 
-  await connection();
+//   await connection();
 
-  try {
-    const hashedPassword = await bcrypt.hash(password, 5);
-    const newUser = await userModel.create({
-      name,
-      email,
-      password: hashedPassword,
-    });
+//   try {
+//     const hashedPassword = await bcrypt.hash(password, 5);
+//     const newUser = await userModel.create({
+//       name,
+//       email,
+//       password: hashedPassword,
+//     });
 
-    res.status(201).json({ success: true, data: newUser });
-  } catch (error: any) {
-    res.status(500).json({ success: false, error: error.message });
-  }
-}
+//     res.status(201).json({ success: true, data: newUser });
+//   } catch (error: any) {
+//     res.status(500).json({ success: false, error: error.message });
+//   }
+// }
