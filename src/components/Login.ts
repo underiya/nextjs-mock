fa1d749a460b8f6d9a015cf134c7ba66bc2a63b3
Deepak Underiya <underiyadeepak@gmail.com>
2024-05-08 23:53:16 +0530

    done

--- src/components/Login.ts
+++ src/components/Login.ts
@@ -1,38 +1,37 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import userModel from "../../backend/models/user.model";
-import { connection } from "@/lib/db";
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
+// import { NextApiRequest, NextApiResponse } from "next";
+// import userModel from "../../backend/models/user.model";
+// const bcrypt = require("bcrypt");
+// const jwt = require("jsonwebtoken");
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  const { email, password } = req.body;
+// export default async function handler(
+//   req: NextApiRequest,
+//   res: NextApiResponse
+// ) {
+//   const { email, password } = req.body;
 
-  await connection();
+//   await connection();
 
-  try {
-    const user = await userModel.findOne({ email });
-    if (!user) {
-      return res
-        .status(404)
-        .json({ success: false, message: "User not found" });
-    }
+//   try {
+//     const user = await userModel.findOne({ email });
+//     if (!user) {
+//       return res
+//         .status(404)
+//         .json({ success: false, message: "User not found" });
+//     }
 
-    const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) {
-      return res
-        .status(401)
-        .json({ success: false, message: "Invalid credentials" });
-    }
+//     const isMatch = await bcrypt.compare(password, user.password);
+//     if (!isMatch) {
+//       return res
+//         .status(401)
+//         .json({ success: false, message: "Invalid credentials" });
+//     }
 
-    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
-      expiresIn: "1h",
-    });
+//     const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+//       expiresIn: "1h",
+//     });
 
-    res.status(200).json({ success: true, token });
-  } catch (error) {
-    res.status(500).json({ success: false, error: error });
-  }
-}
+//     res.status(200).json({ success: true, token });
+//   } catch (error) {
+//     res.status(500).json({ success: false, error: error });
+//   }
+// }
